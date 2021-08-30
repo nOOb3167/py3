0e4e3018981a30ee236abee611739fb8ab3f4314
nOOb3167 <nOOb3167@gmail.com>
2021-08-30 22:15:26 +0200

    stuff

--- dodo.py
+++ dodo.py
@@ -1,9 +1,36 @@
-DOIT_CONFIG = {
-    'default_tasks': ['t0']
-}
+import pathlib
+
+DOIT_CONFIG = {"default_tasks": ["t0"]}
+
 
 def task_t0():
     return {
-        'actions': ['py -m pip list'],
-        'verbosity': 2,
+        "actions": ["py -m pip list"],
+        "verbosity": 2,
+    }
+
+
+def task_clean_dist():
+    def clean_dist():
+        tgz = pathlib.Path(".").glob("dist/*.tar.gz")
+        whl = pathlib.Path(".").glob("dist/*.whl")
+        for f in list(tgz) + list(whl):
+            f.unlink()
+
+    return {
+        "actions": [clean_dist],
+    }
+
+
+def task_build():
+    return {
+        "actions": ["py -m build --sdist --wheel ."],
+        "task_dep": ["clean_dist"],
+    }
+
+
+def task_upload_test():
+    return {
+        "actions": ["py -m twine upload -r testpypi dist/*"],
+        "verbosity": 2,
     }
